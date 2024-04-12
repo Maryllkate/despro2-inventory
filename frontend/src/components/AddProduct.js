e26af14f28b428db06083282c48b12d7be4c682e
Maryllkate <maryllkate20@gmail.com>
2024-04-12 20:14:53 +0800

    addProduct

--- frontend/src/components/AddProduct.js
+++ frontend/src/components/AddProduct.js
@@ -101,10 +101,10 @@ export default function AddProduct({
                               id="name"
                               value={product.rfidID}
                               onChange={(e) =>
-                                handleInputChange(e.target.name, e.target.value)
+                                handleInputChange(e.target.rfidID, e.target.value)
                               }
                               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                              placeholder="Ex. RFID#123"
+                              placeholder="Ex. 123456789"
                             />
                           </div>
                           <div>
