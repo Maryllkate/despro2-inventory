a01d58f84c55cd064e64e232bf3f60336ba4d9e7
Maryllkate <maryllkate20@gmail.com>
2024-04-26 10:12:37 +0800

    stock to 1 only

--- frontend/src/components/AddProduct.js
+++ frontend/src/components/AddProduct.js
@@ -178,13 +178,14 @@ export default function AddProduct({
                               type="number"
                               name="stock"
                               id="stock"
-                              value={product.stock}
+                              value="1"
+                              disabled
                               onChange={(e) =>
                                 handleInputChange("stock", e.target.value) // Pass "stock" as the key
                               }
                               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                              placeholder="0 - 999"
-                            />
+                              
+                            /> 
                           </div>
 
                           <div className="sm:col-span-2">
