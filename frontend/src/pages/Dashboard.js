76b0be375576e15bbbd9b651326dfd9d314e7b27
Maryllkate <maryllkate20@gmail.com>
2024-04-26 09:36:56 +0800

    dashboard up

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -6,11 +6,11 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 export const data = {
-  labels: ["Apple", "Knorr", "Shoop", "Green", "Purple", "Orange"],
+  labels: ["Fruits", "Beverages", "Essentials", "Snacks", "Drinks", "Others"],
   datasets: [
     {
       label: "# of Votes",
-      data: [0, 1, 5, 8, 9, 15],
+      data: [0, 10, 5, 9, 9, 6],
       backgroundColor: [
         "rgba(255, 99, 132, 0.2)",
         "rgba(54, 162, 235, 0.2)",
@@ -165,14 +165,14 @@ function Dashboard() {
                ₱{saleAmount}
               </span>
 
-              <span className="text-xs text-gray-500"> from ₱240.94 </span>
+              <span className="text-xs text-gray-500"> </span>
             </p>
           </div>
         </article>
 
         <article className="flex flex-col  gap-4 rounded-lg border border-gray-100 bg-white p-6 ">
-          <div className="inline-flex gap-2 self-end rounded bg-red-100 p-1 text-red-600">
-            <svg
+          <div className="inline-flex gap-2 self-end rounded bg-green-100 p-1 text-green-600">
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-4 w-4"
               fill="none"
@@ -185,9 +185,24 @@ function Dashboard() {
                 strokeWidth="2"
                 d="M13 17h8m0 0V9m0 8l-8-8-4 4-6-6"
               />
+            </svg> */}
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"
+              />
             </svg>
 
-            <span className="text-xs font-medium"> 67.81% </span>
+            <span className="text-xs font-medium"> 78.81% </span>
           </div>
 
           <div>
@@ -201,13 +216,13 @@ function Dashboard() {
                 ₱{purchaseAmount}{" "}
               </span>
 
-              <span className="text-xs text-gray-500"> from ₱404.32 </span>
+              <span className="text-xs text-gray-500"> </span>
             </p>
           </div>
         </article>
         <article className="flex flex-col   gap-4 rounded-lg border border-gray-100 bg-white p-6 ">
-          <div className="inline-flex gap-2 self-end rounded bg-red-100 p-1 text-red-600">
-            <svg
+          <div className="inline-flex gap-2 self-end rounded bg-green-100 p-1 text-green-600">
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-4 w-4"
               fill="none"
@@ -220,9 +235,24 @@ function Dashboard() {
                 strokeWidth="2"
                 d="M13 17h8m0 0V9m0 8l-8-8-4 4-6-6"
               />
+            </svg> */}
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"
+              />
             </svg>
 
-            <span className="text-xs font-medium"> 67.81% </span>
+            <span className="text-xs font-medium"> 70.81%</span>
           </div>
 
           <div>
@@ -241,8 +271,8 @@ function Dashboard() {
           </div>
         </article>
         <article className="flex flex-col   gap-4 rounded-lg border border-gray-100 bg-white p-6 ">
-          <div className="inline-flex gap-2 self-end rounded bg-red-100 p-1 text-red-600">
-            <svg
+          <div className="inline-flex gap-2 self-end rounded bg-green-100 p-1 text-green-600">
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-4 w-4"
               fill="none"
@@ -255,9 +285,9 @@ function Dashboard() {
                 strokeWidth="2"
                 d="M13 17h8m0 0V9m0 8l-8-8-4 4-6-6"
               />
-            </svg>
+            </svg> */}
 
-            <span className="text-xs font-medium"> 67.81% </span>
+            <span className="text-xs font-medium"> 5% </span>
           </div>
 
           <div>
